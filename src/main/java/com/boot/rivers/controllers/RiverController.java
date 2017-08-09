0e2c8c5d5666d33b188da22864386fa474c91a4b
Dan Bunker <dan.bunker@stgconsulting.com>
2017-08-08 23:52:38 -0700

    Initial checkin

---
+++ src/main/java/com/boot/rivers/controllers/RiverController.java
@@ -0,0 +1,33 @@
+package com.boot.rivers.controllers;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("api/v1/rivers")
+public class RiverController {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<String> list() {
+		HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        String url = "http://riverbrain.com/api/v1/rivers";
+        ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);        
+		return res;
+	}
+
+}
